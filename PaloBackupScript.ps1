4573498e8263c8134e47aa7acbf8c9b0ca266811
Matt A <marsenault89@gmail.com>
2024-11-04 13:33:37 -0500

    Update PaloBackupScript.ps1

--- PaloBackupScript.ps1
+++ PaloBackupScript.ps1
@@ -7,28 +7,28 @@ $Date = (Get-Date).ToString('MM_dd_yyyy')
 
 ## Device State Folder Locations
 
-$destDevice = "\\uncpathhere\Palo Alto Automated Backups\DeviceState"
+$destDevice = "\\<uncpathhere>\Palo Alto Automated Backups\DeviceState"
 ###Copy Past for more locations
 
 ## Running Config Folder locations:
-$destRunning = "\\uncpathhere\Palo Alto Automated Backups\RunningConfig"
+$destRunning = "\\<uncpathhere>\Palo Alto Automated Backups\RunningConfig"
 ###Copy Past for more locations
 
 
 ## Download Device State Config
 
-curl "https://PALO-DNS-NAME/api/?type=export&category=device-state&key=PALO-API-KEY" -OutFile ("$destDevice\GV_DeviceStateConfiguration" + "$Date" +".xml")
+curl "https://<PALO-HPPTS-DNS-NAME>/api/?type=export&category=device-state&key=PALO-API-KEY" -OutFile ("$destDevice\DeviceStateConfiguration" + "$Date" +".xml")
 Start-Sleep -Seconds 10
 
 ## Download running config:
-curl "https://PALO-DNS-NAME/api/?type=export&category=configuration&key=PALO-API-KEY" -OutFile ("$destRunning\GV_RunningConfiguration" + "$Date" +".xml")
+curl "https://<PALO-HTTPS-DNS-NAME>/api/?type=export&category=configuration&key=PALO-API-KEY" -OutFile ("$destRunning\RunningConfiguration" + "$Date" +".xml")
 Start-Sleep -Seconds 10
 
 ## Remove Device State backups older than 30 days:
-Get-ChildItem –Path "\\uncpathhere\Palo Alto Automated Backups\DeviceState" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
+Get-ChildItem –Path "\\<uncpathhere>\Palo Alto Automated Backups\DeviceState" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
 Start-Sleep -Seconds 10
 
 ## Remove Running Config backups older than 30 days:
 
-Get-ChildItem –Path "\\uncpathhere\Palo Alto Automated Backups\RunningConfig" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
+Get-ChildItem –Path "\\<uncpathhere>\Palo Alto Automated Backups\RunningConfig" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item
 Start-Sleep -Seconds 10
